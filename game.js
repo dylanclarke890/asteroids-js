4df6a2b1b51e3178ad2d76977d4b82e4a2ed0497
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-01 23:52:10 +0100

    added controlled animation loop

--- game.js
+++ game.js
@@ -162,13 +162,38 @@ const state = {
   player: new Player(),
 };
 
+const FPS = 60;
+const settings = {
+  fps: FPS,
+  fpsInterval: 1000 / FPS,
+};
+
 function handleObjects() {
   state.player.update();
   state.player.draw();
 }
 
-(function animate() {
+function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   handleObjects();
-  requestAnimationFrame(animate);
+}
+
+let stop = false,
+  now,
+  lastFrame;
+
+(function startAnimating() {
+  lastFrame = window.performance.now();
+  animate();
 })();
+
+function animate(newtime) {
+  if (stop) return;
+  requestAnimationFrame(animate);
+  now = newtime;
+  const elapsed = now - lastFrame;
+  if (elapsed > settings.fpsInterval) {
+    lastFrame = now - (elapsed % settings.fpsInterval);
+    update();
+  }
+}
